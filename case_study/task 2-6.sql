ed0fa2751c0227b6fe172078b5b72b5d4f22cead
QuangTruong224 <changtraitre2000@gmail.com>
2022-04-02 19:27:23 +0700

    push case 2-6

--- case_study/task 2-6.sql
+++ case_study/task 2-6.sql
@@ -39,11 +39,11 @@ sum(dich_vu.chi_phi_thue + (hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia))) a
 from khach_hang
 left join loai_khach on khach_hang.ma_loai_khach = loai_khach.ma_loai_khach
 left join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
-left join dich_vu ON hop_dong.ma_dich_vu =  dich_vu.ma_dich_vu
-left join hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
-left join dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+left join dich_vu on hop_dong.ma_dich_vu =  dich_vu.ma_dich_vu
+left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
 group by hop_dong.ma_hop_dong
-order by  khach_hang.ma_khach_hang , hop_dong.ma_hop_dong DESC ;
+order by  khach_hang.ma_khach_hang , hop_dong.ma_hop_dong desc ;
 
  -- task 6 Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
 --  của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
@@ -58,6 +58,9 @@ between "2021/01/01" and"2021/03/31")
 group by ma_dich_vu order by chi_phi_thue desc; 
 
 -- task 8	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+-- cách 1: 
 select  khach_hang.ho_ten from khach_hang
 union 
 select  khach_hang.ho_ten from khach_hang;
+-- cách 2: 
+ select distinct khach_hang.ho_ten from khach_hang;
