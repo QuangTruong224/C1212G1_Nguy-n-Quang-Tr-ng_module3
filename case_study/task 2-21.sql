07a9ee46b034a1e35a6e21973744d1d604dce48f
QuangTruong224 <changtraitre2000@gmail.com>
2022-04-04 18:16:31 +0700

    task 15-21

--- case_study/task 2-21.sql
+++ case_study/task 2-21.sql
@@ -136,7 +136,27 @@ where
 -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem),
 --  tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
 --  nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
-
+ select
+  hop_dong.ma_hop_dong , nhan_vien.ho_ten , khach_hang.ho_ten , khach_hang.so_dien_thoai , dich_vu.ten_dich_vu , dich_vu.ma_dich_vu,
+  sum(ifnull(so_luong,0)) as so_luong_dich_vu_di_kem,
+  hop_dong.tien_dat_coc
+  from 
+	khach_hang 
+    join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
+    join nhan_vien on hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
+    join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+    left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+    left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+    where 
+    hop_dong.ma_hop_dong in 
+    (select ma_hop_dong from hop_dong 
+    where (month(ngay_lam_hop_dong)between 10 and 12 )
+			and year(ngay_lam_hop_dong)=2020)
+	and hop_dong.ma_hop_dong not in 
+    (select ma_hop_dong from hop_dong 
+    where (month(ngay_lam_hop_dong)between 1 and 6 )
+			and year(ngay_lam_hop_dong)=2021)
+group by ma_hop_dong ;
 -- task 13 Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng
 -- . (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
 select 
