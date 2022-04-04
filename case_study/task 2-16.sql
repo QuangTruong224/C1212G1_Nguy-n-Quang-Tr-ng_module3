6f09d0974615548f9bcc2502569d4e306d6eb707
QuangTruong224 <changtraitre2000@gmail.com>
2022-04-04 10:17:59 +0700

    task 8-15

--- case_study/task 2-16.sql
+++ case_study/task 2-16.sql
@@ -57,7 +57,8 @@ where  hop_dong.ngay_lam_hop_dong
 between "2021/01/01" and"2021/03/31")
 group by ma_dich_vu order by chi_phi_thue desc; 
 
--- task 7: 
+-- task 7: Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu 
+-- của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
  select 
  dich_vu.ma_dich_vu,
  dich_vu.ten_dich_vu,
@@ -130,32 +131,6 @@ where
 -- (được tính dựa trên việc sum so_luong ở dich_vu_di_kem),
 --  tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 
 --  nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021.
- select
-  hop_dong.ma_hop_dong,
-  nhan_vien.ho_ten,
-  khach_hang.ho_ten,
-  khach_hang.so_dien_thoai,
-  dich_vu.ten_dich_vu,
-  dich_vu.ma_dich_vu,
-  sum(ifnull(so_luong,0)) as so_luong_dich_vu_di_kem,
-  hop_dong.tien_dat_coc
-  from 
-	khach_hang 
-    join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
-    join nhan_vien on hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
-    join dich_vu on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
-    left join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
-    left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
-    where 
-    hop_dong.ma_hop_dong in 
-    (select ma_hop_dong from hop_dong 
-    where (month(ngay_lam_hop_dong)between 10 and 12 )
-			and year(ngay_lam_hop_dong)=2020)
-	and hop_dong.ma_hop_dong not in 
-    (select ma_hop_dong from hop_dong 
-    where (month(ngay_lam_hop_dong)between 1 and 6 )
-			and year(ngay_lam_hop_dong)=2021)
-group by ma_hop_dong ;
 
 -- task 13 Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng
 -- . (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
@@ -168,5 +143,48 @@ join hop_dong_chi_tiet on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.m
 group by hop_dong_chi_tiet.ma_dich_vu_di_kem
 having so_luong_dich_vu_di_kem >= all(select so_luong 
 from hop_dong_chi_tiet);
+
+-- task 14 	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất.
+--  Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung
+--  (được tính dựa trên việc count các ma_dich_vu_di_kem). 
+select hop_dong.ma_hop_dong , loai_dich_vu.ten_loai_dich_vu , dich_vu_di_kem.ten_dich_vu_di_kem, count(dich_vu_di_kem.ma_dich_vu_di_kem)
+from hop_dong 
+inner join dich_vu on hop_dong.ma_dich_vu=dich_vu.ma_dich_vu
+inner join loai_dich_vu on  dich_vu.ma_loai_dich_vu= loai_dich_vu.ma_loai_dich_vu
+inner join hop_dong_chi_tiet on hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+inner join dich_vu_di_kem on  hop_dong_chi_tiet.ma_dich_vu_di_kem= dich_vu_di_kem.ma_dich_vu_di_kem
+group by dich_vu_di_kem.ma_dich_vu_di_kem
+having
+count(dich_vu_di_kem.ma_dich_vu_di_kem)=1
+order by hop_dong.ma_hop_dong asc
+; 
+
+-- task 15 Hiển thi thông tin của tất cả nhân viên bao gồm
+--  ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select nhan_vien.ma_nhan_vien,nhan_vien.ho_ten, trinh_do.ten_trinh_do , bo_phan.ten_bo_phan, nhan_vien.so_dien_thoai, nhan_vien.dia_chi
+from nhan_vien
+inner join vi_tri on nhan_vien.ma_vi_tri=vi_tri.ma_vi_tri
+inner join trinh_do on nhan_vien.ma_trinh_do = trinh_do.ma_trinh_do
+inner join bo_phan on nhan_vien.ma_bo_phan=bo_phan.ma_bo_phan
+left join hop_dong on nhan_vien.ma_nhan_vien = hop_dong.ma_nhan_vien
+where (year(hop_dong.ngay_lam_hop_dong) between 2020 and 2021) 
+group by nhan_vien.ma_nhan_vien 
+having
+count(hop_dong.ngay_lam_hop_dong) < 4;
+
+-- task 16 Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+-- select nhan_vien.ma_nhan_vien, nhan_vien.ho_ten
+-- from nhan_vien
+-- inner join hop_dong on nhan_vien.ma_hop_dong=hop_dong.ma_hop_dong
+-- inner join 
+
+-- -- task 17 Cập nhật thông tin những khách hàng có ten_loai_khach từ Platinum lên Diamond, 
+-- -- chỉ cập nhật những khách hàng đã từng đặt phòng với Tổng Tiền thanh toán trong năm 2021 là lớn hơn 10.000.000 VNĐ.
+-- select loai_khach.ma_loai_khach, loai_khach.ten_loai_khach
+-- from loai_khach 
+
+ 
+
+
 
  
